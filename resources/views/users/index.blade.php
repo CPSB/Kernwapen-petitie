57f45d77fde2acf306672755368cd886437bada3
Tjoosten <Tjoosten3@gmail.com>
2017-11-29 14:33:41 +0100

    Work on users index view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.backend')
 
 @section('content')
     <div class="container">
@@ -12,15 +12,50 @@
 
                         <div class="pull-right">
                             @if (count($users) > 10)
-                                <a class="btn btn-default btn-sm" href="">
+                                <a class="btn btn-default btn-xs" href="">
                                     <i class="fa fa-search"></i> Gebruiker zoeken.
                                 </a>
                             @endif
 
-                            <a class="btn btn-default btn-sm" href="">
+                            <a class="btn btn-default btn-xs" href="">
                                 <i class="fa fa-user-plus"></i> Gebruiker toevoegen.
                             </a>
                         </div>
+                    </div>
+
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-condensed table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Status</th>
+                                        <th>Naam:</th>
+                                        <th>Email:</th>
+                                        <th colspan="2">Aangemaakt op:</th> {{-- Colspan="2" nodig voor de functies --}}
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if (count($users) == 0) {{-- No users found --}}
+                                        <tr>
+                                            <td colspan="6"><i>(Er zijn geen gebruikers gevonden.)</i></td>
+                                        </tr>
+                                    @else {{-- There are users found --}}
+                                        @foreach ($users as $user) {{-- Loop through the users --}}
+                                            <tr>
+                                                <td><strong>#{{ $user->id }}</strong></td>
+                                                <td></td> {{-- TODO: Implement status indicater (active/ban) --}}
+                                                <td>{{ $user->name }}</td>
+                                                <td>{{ $user->email }}</td>
+                                                <td>{{ $user->created_at->diffForHumans() }}</td>
+                                            </tr>
+                                        @endforeach {{-- END loop --}}
+                                    @endif
+                                </tbody>
+                            </table>
+                        </div>
+
+                        {{ $users->render() }}
                     </div>
                 </div>
             </div>
